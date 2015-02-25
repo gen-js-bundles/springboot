f580e013d9fd622bc6d525f47f260c0712e57cd3
Ludovic Chaboud <l.chaboud@gmail.com>
2015-02-25 11:10:17 +0100

    Update init.js

--- tasks/init.js
+++ tasks/init.js
@@ -27,6 +27,11 @@ module.exports = {
           path.join(__dirname,'../model/config.@maven.yml'),
           path.join(process.cwd(),'model/config.@maven.yml'));
       }
+      if(answers.buildTool == 'gradle') {
+        gfile.copy(
+          path.join(__dirname,'../model/config.@gradle.yml'),
+          path.join(process.cwd(),'model/config.@gradle.yml'));
+      }
     });
   }
 };
