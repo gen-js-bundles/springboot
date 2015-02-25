213cb5260205b6603414dc7f21c6a2ec01f7e8a3
Ludovic Chaboud <l.chaboud@gmail.com>
2015-02-25 13:45:51 +0100

    Update init.js

--- tasks/init.js
+++ tasks/init.js
@@ -2,7 +2,8 @@ var
   inquirer = require("inquirer"),
   fs = require('fs'),
   path = require('path'),
-  gfile = require('gfilesync');
+  gfile = require('gfilesync'),
+  yaml = require('js-yaml');
 
 module.exports = {
   askFor: function() {
@@ -32,6 +33,50 @@ module.exports = {
           path.join(__dirname,'../model/config.@gradle.yml'),
           path.join(process.cwd(),'model/config.@gradle.yml'));
       }
+
+      var data = gfile.loadYaml(path.join(process.cwd(),'Genjsfile.yml'));
+
+      if(data.global == null) {
+        data.global = {};
+      }
+      if(data.global.project == null) {
+        data.global.project = {};
+      }
+      if(data.global.project.name == null) {
+        data.global.project.name = 'myapp';
+      }
+      if(data.global.project.version == null) {
+        data.global.project.version = '0.1';
+      }
+      if(data.global.project.description == null) {
+        data.global.project.description = '';
+      }
+
+      if(data.global.maven == null) {
+        data.global.maven = {};
+      }
+      if(data.global.maven.groupId == null) {
+        data.global.maven.groupId = 'demo';
+      }
+      if(data.global.maven.artifactId == null) {
+        data.global.maven.artifactId = 'myapp';
+      }
+      if(data.global.maven.packaging == null) {
+        data.global.maven.packaging = 'war'
+      }
+
+      if(data.global.version == null) {
+        data.global.version = {};
+      }
+      if(data.global.version.springboot == null) {
+        data.global.version.springboot = '1.2.1';
+      }
+      if(data.global.version.java == null) {
+        data.global.version.java = '1.8';
+      }
+
+      gfile.writeYaml(path.join(process.cwd(),'Genjsfile.yml'), data);
+
     });
   }
 };
