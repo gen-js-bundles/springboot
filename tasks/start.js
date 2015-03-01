761c962e8775565c72d2c7b9da0e720e5b270e5e
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-01 17:17:54 +0100

    Update and rename tasks to tasks/start.js

---
+++ tasks/start.js
@@ -0,0 +1,26 @@
+var
+  inquirer = require("inquirer"),
+  fs = require('fs'),
+  path = require('path');
+
+module.exports = {
+  do: function(callback) {
+
+    // Start
+    var questions = [
+      {
+        type: 'input',
+        name: 'start',
+        message: 'Start the application ?',
+        default: true
+      }
+    ];
+    inquirer.prompt(questions, function( answers ) {
+
+      if(answers.start) {
+        console.log('start');
+      }
+
+    }):
+  }
+};
