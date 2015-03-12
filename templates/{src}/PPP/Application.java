be63180133adb6caf6c41ab8ad09d19868617002
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-12 16:20:05 +0100

    Update Application.java

--- templates/{src}/PPP/Application.java
+++ templates/{src}/PPP/Application.java
@@ -2,8 +2,12 @@ package <%= gen.package %>;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+<% if(has(all.config.tags.build.dependencies, 'artifactId', 'spring-cloud-starter-eureka-server')) { %>
+@EnableEurekaClient
+<% } %>
 public class Application {
 
     public static void main(String[] args) {
