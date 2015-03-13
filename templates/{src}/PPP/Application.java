b51eaf611c7881319eeb98106a4a7db656445d1d
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-13 17:31:09 +0100

    Update Application.java

--- templates/{src}/PPP/Application.java
+++ templates/{src}/PPP/Application.java
@@ -2,7 +2,10 @@ package <%= gen.package %>;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+<% if(has(all.config.tags.build.dependencies, 'artifactId', 'spring-cloud-starter-eureka-server')) { %>
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+<% } %>
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @SpringBootApplication
 <% if(has(all.config.tags.build.dependencies, 'artifactId', 'spring-cloud-starter-eureka-server')) { %>
