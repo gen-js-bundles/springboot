97a73988ca30217c15335c3db733a3dbd52806ae
Ludovic Chaboud <l.chaboud@gmail.com>
2015-03-20 12:09:23 +0100

    Update init.js

--- tasks/init.js
+++ tasks/init.js
@@ -39,7 +39,7 @@ module.exports = {
           artifactId: "spring-cloud-starter-hystrix",
           isSpringCloud: true
         }
-      }
+      },
       {
         name: "Spring Cloud - Config Client",
         value: {
